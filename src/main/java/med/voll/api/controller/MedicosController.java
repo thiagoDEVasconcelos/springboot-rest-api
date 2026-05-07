4501f056d31b3339c15f8fc8e52cd9fa2b65830c
Thiago Sousa <thiagovsousa21@gmail.com>
2026-05-07 17:58:44 -0300

    Refactoring

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DadosMedicoRegistrado;
+import med.voll.api.medico.DadosMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicosController {
 
     @PostMapping
-    public void register(@RequestBody DadosMedicoRegistrado data) {
+    public void register(@RequestBody DadosMedico data) {
         System.out.println(data);
     }
 
--- src/main/java/med/voll/api/medico/DadosMedicoRegistrado.java
+++
@@ -1,3 +0,0 @@
-package med.voll.api.medico;
-
-public record DadosMedicoRegistrado(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {}
