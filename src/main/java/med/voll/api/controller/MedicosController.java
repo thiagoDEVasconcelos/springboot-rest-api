685ec7dd537db86caff33399ac06fac9b608a6a7
Thiago Sousa <thiagovsousa21@gmail.com>
2026-05-08 14:19:18 -0300

    Creating the get methods to list all medicos.

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,13 +1,14 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.DadosMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -16,8 +17,13 @@ public class MedicosController {
     @Autowired
     private MedicoRepository repository;
 
+    @GetMapping
+    public List<DadosListagemMedicos> list() {
+        return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+    }
+
     @PostMapping
-    public void register(@RequestBody DadosMedico dados) {
+    public void register(@RequestBody @Valid DadosMedico dados) {
         repository.save(new Medico(dados));
     }
 
