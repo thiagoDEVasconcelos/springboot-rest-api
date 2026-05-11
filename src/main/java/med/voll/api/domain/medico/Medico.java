5c2e6f478122dcbbf16e6256d8161c35b0e5e4e3
Thiago Sousa <thiagovsousa21@gmail.com>
2026-05-11 20:03:23 -0300

    Refactoring.

--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,10 +1,10 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 import jakarta.persistence.*;
 
 @Table(name = "medicos")
