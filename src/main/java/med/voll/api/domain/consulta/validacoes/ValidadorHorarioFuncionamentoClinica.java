fa122487e028af532a1e53ad9d9916b089c6f0fa
Thiago Sousa <thiagovsousa21@gmail.com>
2026-05-21 01:55:49 -0300

    Validating consultas.

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDoHorarioDeAbertura = dataConsulta.getHour() < 7;
+        var depoisDoHorarioDeEncerramento = dataConsulta.getHour() > 18;
+
+        if(domingo || antesDoHorarioDeAbertura || depoisDoHorarioDeEncerramento) {
+            throw new ValidationException("Consulta fora do horário de funcionamento da clínica");
+        }
+    }
+}
